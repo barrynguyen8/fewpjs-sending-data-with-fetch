43321c0c1e13ab91a475dbcb49e406502ea356d0
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-03-26 14:12:04 +1100

    assignment submitted

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+// const dogForm = document.getElementById('dogForm');
+// const dogName = document.getElementById('dogName');
+// const dogBreed = document.getElementById('dogBreed');
+
+function submitData(name, email) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name, 
+            email: email 
+        })
+    };
+
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then(function(response){return response.json()})
+        .then(function(result){document.body.innerHTML = result['id']}) 
+        .catch(error => document.body.innerHTML=error.message);
+};
+
+submitData(); 
+
