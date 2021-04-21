22260aa68b6f481af424c56af2f23e76f3ec9df6
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-04-21 14:37:33 +1000

    assignment submitted

--- index.js
+++ index.js
@@ -3,24 +3,47 @@
 // const dogName = document.getElementById('dogName');
 // const dogBreed = document.getElementById('dogBreed');
 
-function submitData(name, email) {
-    const configurationObject = {
+// function submitData(name, email) {
+//     const configurationObject = {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json"
+//         },
+//         body: JSON.stringify({
+//             name: name, 
+//             email: email 
+//         })
+//     };
+
+//     return fetch("http://localhost:3000/users", configurationObject)
+//         .then(function(response){return response.json()})
+//         .then(function(result){document.body.innerHTML = result['id']}) 
+//         .catch(error => document.body.innerHTML=error.message);
+// };
+
+// submitData(); 
+
+function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
         method: "POST",
         headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
+          "Content-Type": "application/json",
+          "Accept": "application/json"
         },
-        body: JSON.stringify({
-            name: name, 
-            email: email 
-        })
-    };
-
-    return fetch("http://localhost:3000/users", configurationObject)
-        .then(function(response){return response.json()})
-        .then(function(result){document.body.innerHTML = result['id']}) 
-        .catch(error => document.body.innerHTML=error.message);
-};
-
-submitData(); 
-
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+      .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }
+  
